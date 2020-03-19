cfea2f2e3980371d53cd930e8e350e7faf612b52
Ethic Hadebe <ethichadebe@gmail.com>
2020-03-19 04:50:50 +0200

    post register function complete

--- api/routes/users.js
+++ api/routes/users.js
@@ -11,31 +11,31 @@ const conn = mysql.createConnection({
 });
 
 //Returns all users
-router.get('/', (req, res, next) =>{
+router.get('/', (req, res, next) => {
     conn.query("SELECT * FROM users", (err, rows, fields) => {
         res.json({
-            users:rows
+            users: rows
         });
     });
 });
 
 //insert a user
-router.post('/', (req, res, next) =>{
+router.post('/', (req, res, next) => {
     res.status(200).json({
         message: 'POST request'
     })
 });
 
 //Returns a specific user 
-router.post('/Login', (req, res) =>{
-    //const userID = req.params.id;
-    conn.query("SELECT * FROM users WHERE uNumber = ? AND uPassword = ?",[req.body.uNumber, req.body.uPassword], (err, result, fields) => {
-        console.log(result + " " + req.body.uNumber + " " + req.body.uPassword);
-        if(result.length >0){
+router.post('/Login', (req, res) => {
+    const selectQuery = "SELECT * FROM users WHERE uNumber = ? AND uPassword = ?";
+    conn.query(selectQuery, [req.body.uNumber, req.body.uPassword], (err, result, fields) => {
+        console.log(result)// + " " + req.body.uNumber + " " + req.body.uPassword);
+        if (result.length > 0) {
             res.json({
                 data: result
             })
-        }else{
+        } else {
             res.json({
                 data: "false"
             })
@@ -44,22 +44,49 @@ router.post('/Login', (req, res) =>{
 });
 
 //User registration 
-router.post('/Register', (req, res) =>{
-    //const userID = req.params.id;
-    const insQuery = "INSERT INTO users(`uName`, `uSurname`,`uDOB`,`uSex`,`uEmail`,`uNumber`,`uPassword`,`isActive`) VALUES (?, ?,?, ?, ?, ?,?,1)"
-    conn.query(insQuery,[req.body.uName, req.body.uSurname, req.body.uDOB, req.body.uSex, req.body.uEmail, 
-                        req.body.uNumber, req.body.uPassword], (err, result, fields) => {
-        console.log(err);
-        console.log(result);
-        if(err){
+router.post('/Register', (req, res) => {
+    const insQuery = "INSERT INTO users(`uName`, `uSurname`,`uDOB`,`uSex`,`uEmail`,`uNumber`,`uPassword`,`isActive`) VALUES (?, ?,?, ?, ?, ?,?,1)";
+    const selectQuery = "SELECT * FROM users WHERE uNumber = ? AND uEmail = ?";
+    const selectQuery1 = "SELECT * FROM users WHERE uNumber = ?";
+    const selectQuery2 = "SELECT * FROM users WHERE uEmail = ?";
+
+    //Check if number and email exists
+    conn.query(selectQuery, [req.body.uNumber, req.body.uEmail], (err, result, fields) => {
+        if (result.length > 0) {
+            console.log(result);
             res.json({
-                message: "something went wrong"
-            })
-        }   
-        res.json({
-            message: "Registered"
-        })              
+                data: "both"
+            });
+        } else {
+            //Check if number exists
+            conn.query(selectQuery1, [req.body.uNumber], (err, result, fields) => {
+                if (result.length > 0) {
+                    res.json({
+                        data: "number"
+                    });
+                } else {
+                    //Check if email exists
+                    conn.query(selectQuery2, [req.body.uEmail], (err, result, fields) => {
+                        if (result.length > 0) {
+                            res.json({
+                                data: "email"
+                            })
+                        } else {
+                            conn.query(insQuery, [req.body.uName, req.body.uSurname, req.body.uDOB, req.body.uSex, req.body.uEmail,
+                            req.body.uNumber, req.body.uPassword], (err, result, fields) => {
+                                console.log(err);
+                                console.log(result);
+                                res.json({
+                                    data: "Registered"
+                                })
+                            });
+                        };
+                    });
+                }
+            });
+        }
     });
+
 });
 
 module.exports = router;
