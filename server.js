8b92d179855ae7c81015346bd80e25fe7ec3f0c1
Ethic Hadebe <ethichadebe@gmail.com>
2020-05-20 02:39:06 +0200

    trying a util file

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = require('./app');
 
 const port = process.env.PORT || 3005;
 const server = http.createServer(app);
+require('babel-register')({
+    presets: ['env']
+});
 
 server.listen(port, () => {
     console.log("server started");
 });    
+
+module.exports = require('./app.js')
