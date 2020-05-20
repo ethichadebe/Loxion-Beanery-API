04de11caf2ffb671cff7bf1c3d90285c04cc8290
Ethic Hadebe <ethichadebe@gmail.com>
2020-05-20 03:16:06 +0200

    trying a util file

--- util/util.js
+++ util/util.js
@@ -1,4 +1,21 @@
 const mysql = require('mysql');
+//Database connection
+const conn = mysql.createConnection({
+    host: 'sql7.freesqldatabase.com',
+    user: 'sql7339875',
+    password: 'tcyCE9lpMR',
+    database: 'sql7339875',
+    port: '3306'
+});
+
+//add zero to signle digit date and times
+function addZero(data) {
+    if (data.length < 2) {
+        return "0" + data;
+    }
+    return data;
+}
+
 module.exports = {
     //Current date time generator
     createdAt: function () {
@@ -15,23 +32,4 @@ module.exports = {
         return conn;
     }
 };
-//Database connection
-const conn = mysql.createConnection({
-    host: 'sql7.freesqldatabase.com',
-    user: 'sql7339875',
-    password: 'tcyCE9lpMR',
-    database: 'sql7339875',
-    port: '3306'
-});
-
-
-
-//add zero to signle digit date and times
-function addZero(data) {
-    if (data.length < 2) {
-        return "0" + data;
-    }
-    return data;
-}
-
 
