fb7b395b0815c771f1c6dffe456a8fb940159e8e
Ethic Hadebe <ethichadebe@gmail.com>
2020-06-02 02:15:06 +0200

    uploading image

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const router = express.Router();
 
 const helperMethods = require('../../util/util');
-const upload = multer({ dest: 'Uploads/' })
+const upload = multer({ dest: 'Uploads/' });
 //Returns all users
 router.get('/', (req, res, next) => {
     //console.log(helperMethods.conn());
@@ -48,7 +48,7 @@ router.post('/Login', (req, res) => {
 });
 
 //New user registration 
-router.post('/Register',upload.single('ProfilePicture'), (req, res, next) => {
+router.post('/Register', (req, res, next) => {
     console.log(req.file);
     const insQuery = "INSERT INTO users(`uName`, `uSurname`,`uDOB`,`uSex`,`uEmail`,`uNumber`,`uPassword`,`uType`,`isActive`) VALUES (?, ?,?, ?, ?, ?,?,?,1)";
 
