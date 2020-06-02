ff33eabffb84b08700ab3f7999f35ab772aa8d65
Ethic Hadebe <ethichadebe@gmail.com>
2020-06-02 22:16:37 +0200

    Uploading images

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const shopRoutes = require('./api/routes/shops');
 const orderRoutes = require('./api/routes/orders');
 const shopLikesRoutes = require('./api/routes/shoplikes');
 
-app.use('/Uploads', express.static('Uploads'));
+app.use(morgan('combined'));
+app.use(express.static('Uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(morgan('combined'));
 
 app.use('/users', userRoutes);
 app.use('/shops', shopRoutes);
