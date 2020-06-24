5a7eccd0c5c820d01ca22b9d49ada4878911f0d3
Ethic Mthobisi Hadebe <ethichadebe@gmail.com>
2020-06-24 06:12:23 +0200

    POST order update

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -141,7 +141,6 @@ router.post('/Order', (req, res, next) => {
                     const message = {
                         "token":"d7aQZEHUT1i49mbvsXXt8l:APA91bH7Js1Ul9bIoOT-TpMZ-V6QyDxLP04sD3PrUfMJS3GTFyrrYiCK6O7he_BpOpaN1tzEWsYIIviQ3jWBrRMr-V5bV00ZyrSdeUDBNjx_0_51uAUTAL8pgfyBeM_p2DbWNe9G_rTm",
                         "android":{
-                            "ttl":"86400s",
                             "notification":{
                                 "title": "Order #13",
                                 "body": "Not sure yet what to put here"
