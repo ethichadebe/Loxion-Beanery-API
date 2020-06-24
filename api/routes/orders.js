ed670c6eba446a83658983b3b7ea1aad377db5f5
Ethic Mthobisi Hadebe <ethichadebe@gmail.com>
2020-06-24 06:25:04 +0200

    POST order update

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -142,15 +142,13 @@ router.post('/Order', (req, res, next) => {
                         "token": "d7aQZEHUT1i49mbvsXXt8l:APA91bH7Js1Ul9bIoOT-TpMZ-V6QyDxLP04sD3PrUfMJS3GTFyrrYiCK6O7he_BpOpaN1tzEWsYIIviQ3jWBrRMr-V5bV00ZyrSdeUDBNjx_0_51uAUTAL8pgfyBeM_p2DbWNe9G_rTm",
                         "android": {
                             "notification": {
-                                "title": "Order #13",
-                                "body": "Ayispani lento",
-                                "click_action":"OPEN_ACTIVITY_1"
+                                "title": "order: #" + result[0].oNumber,
+                                "body": "R" + result[0].oPrice + " " + result[0].oIngredients,
+                                "click_action": "OPEN_ACTIVITY_1"
                             }
                         },
 
                         "data": {
-                            "oNumber": "order: #" + result[0].oNumber,
-                            "order": "R" + result[0].oPrice + " " + result[0].oIngredients
                         }
                     };
 
