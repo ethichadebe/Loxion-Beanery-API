7b56924f59d6101b60e151c806514100a2da6c93
Ethic Mthobisi Hadebe <ethichadebe@gmail.com>
2020-06-26 07:12:32 +0200

    Updates: Push notifications

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -42,7 +42,7 @@ router.post('/Order', (req, res, next) => {
                     };
 
                     //Send notification
-                    helperMethods.sendNotification(message, res.json({ data: result[0] }));
+                    helperMethods.sendNotification(message, res.json(result[0]));
                 }
             });
         }
@@ -173,63 +173,59 @@ router.put('/Arrived/:oID', (req, res, next) => {
 
     helperMethods.conn().query(putQuery, [req.params.oID], (err, result, fields) => {
         console.log(err);
-        if (!err) {
-            console.log("new order");
-            console.log(result[0]);
-            const selQuery = "SELECT * FROM orders WHERE oID = " + insertedID;
-            helperMethods.conn().query(selQuery, (err, result, fields) => {
-                console.log(err);
-                if (!err) {
-                    console.log("new order");
-                    console.log(result[0]);
-                    //Prepare notification
-                    //TODO: set topic to anyone subscribed to the shop ID
-                    const message = {
-                        "token": "d7aQZEHUT1i49mbvsXXt8l:APA91bH7Js1Ul9bIoOT-TpMZ-V6QyDxLP04sD3PrUfMJS3GTFyrrYiCK6O7he_BpOpaN1tzEWsYIIviQ3jWBrRMr-V5bV00ZyrSdeUDBNjx_0_51uAUTAL8pgfyBeM_p2DbWNe9G_rTm",
-                        "android": {
-                            "notification": {
-                                "title": "Order: #" + result[0].oNumber,
-                                "body": "R" + result[0].oPrice + " " + result[0].oIngredients,
-                                "click_action": "OPEN_ACTIVITY_1"
-                            }
-                        },
-
-                        "data": {
-                            "sID": "" + result[0].sID,
-                            "sName": result[0].sName,
-                            "sShortDescrption": result[0].sShortDescrption,
-                            "sFullDescription": result[0].sFullDescription,
-                            "sSmallPicture": result[0].sSmallPicture,
-                            "sBigPicture": result[0].sBigPicture,
-                            "sLatitude": "" + result[0].sLatitude,
-                            "sLongitude": "" + result[0].sLongitude,
-                            "sAddress": result[0].sAddress,
-                            "sAveTime": "" + result[0].oID,
-                            "sRating": "" + result[0].sRating,
-                            "sStatus": "" + result[0].sStatus,
-                            "sLikes": "" + result[0].sLikes,
-                            "sOperatingHrs": result[0].sOperatingHrs,
-                            "isActive": "" + result[0].isActive,
-                            "sCreatedAt": "" + result[0].sCreatedAt,
-                            "oID": "" + result[0].oID,
-                            "oIngredients": result[0].oIngredients,
-                            "oExtras": result[0].oExtras,
-                            "oPrice": "" + result[0].oPrice,
-                            "oStatus": result[0].oStatus,
-                            "oNumber": "" + result[0].oNumber,
-                            "sID": "" + result[0].sID,
-                            "uID": "" + result[0].uID,
-                            "oCreatedAt": "" + result[0].oCreatedAt,
-                            "oRecievedAt": "" + result[0].oRecievedAt,
-                            "oFinishedAt": "" + result[0].oFinishedAt
+        const selQuery = "SELECT * FROM orders WHERE oID = " + insertedID;
+        helperMethods.conn().query(selQuery, (err, result, fields) => {
+            console.log(err);
+            if (!err) {
+                console.log("new order");
+                console.log(result[0]);
+                //Prepare notification
+                //TODO: set topic to anyone subscribed to the shop ID
+                const message = {
+                    "token": "d7aQZEHUT1i49mbvsXXt8l:APA91bH7Js1Ul9bIoOT-TpMZ-V6QyDxLP04sD3PrUfMJS3GTFyrrYiCK6O7he_BpOpaN1tzEWsYIIviQ3jWBrRMr-V5bV00ZyrSdeUDBNjx_0_51uAUTAL8pgfyBeM_p2DbWNe9G_rTm",
+                    "android": {
+                        "notification": {
+                            "title": "Order: #" + result[0].oNumber,
+                            "body": "R" + result[0].oPrice + " " + result[0].oIngredients,
+                            "click_action": "OPEN_ACTIVITY_1"
                         }
-                    };
+                    },
 
-                    //Send notification
-                    helperMethods.sendNotification(message, res.json({ data: "updated" }));
-                }
-            });
-        }
+                    "data": {
+                        "sID": "" + result[0].sID,
+                        "sName": result[0].sName,
+                        "sShortDescrption": result[0].sShortDescrption,
+                        "sFullDescription": result[0].sFullDescription,
+                        "sSmallPicture": result[0].sSmallPicture,
+                        "sBigPicture": result[0].sBigPicture,
+                        "sLatitude": "" + result[0].sLatitude,
+                        "sLongitude": "" + result[0].sLongitude,
+                        "sAddress": result[0].sAddress,
+                        "sAveTime": "" + result[0].oID,
+                        "sRating": "" + result[0].sRating,
+                        "sStatus": "" + result[0].sStatus,
+                        "sLikes": "" + result[0].sLikes,
+                        "sOperatingHrs": result[0].sOperatingHrs,
+                        "isActive": "" + result[0].isActive,
+                        "sCreatedAt": "" + result[0].sCreatedAt,
+                        "oID": "" + result[0].oID,
+                        "oIngredients": result[0].oIngredients,
+                        "oExtras": result[0].oExtras,
+                        "oPrice": "" + result[0].oPrice,
+                        "oStatus": result[0].oStatus,
+                        "oNumber": "" + result[0].oNumber,
+                        "sID": "" + result[0].sID,
+                        "uID": "" + result[0].uID,
+                        "oCreatedAt": "" + result[0].oCreatedAt,
+                        "oRecievedAt": "" + result[0].oRecievedAt,
+                        "oFinishedAt": "" + result[0].oFinishedAt
+                    }
+                };
+
+                //Send notification
+                helperMethods.sendNotification(message, res.json({ data: "updated" }));
+            }
+        });
     });
 });
 
