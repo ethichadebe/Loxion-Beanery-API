f120b6744a9f5703b28c8fbc0fd8213067388f7c
Ethic Mthobisi Hadebe <ethichadebe@gmail.com>
2020-06-28 01:57:35 +0200

    Updates: Insert Shop notifications, receiver

--- api/routes/shops.js
+++ api/routes/shops.js
@@ -20,95 +20,95 @@ helperMethods.router().get('/:uID/:sLatitude/:sLongitude', (req, res, next) => {
 
 //Returns all shops owned by user
 helperMethods.router().get('/MyShops/:uID', (req, res, next) => {
-    helperMethods.conn().query("SELECT shops.*, usershopbridge.uRole, (SELECT COUNT(*) FROM orders WHERE (shops.sID = orders.sID) AND (orders.oStatus = 'Waiting for order')) AS nOrders FROM shops, usershopbridge WHERE (shops.sID = usershopbridge.sID) AND (usershopbridge.uID = ?) AND (shops.isActive != 2) ORDER BY shops.sStatus DESC", [req.params.uID], (err, rows, fields) => {//WHERE uID=?", 
-    	console.log(err);
-    	console.log(rows);
-    	if (rows.length > 0) {
-    		res.json({
-    			message: "shops",
-    			shops: rows
-    		})
-    	} else {
-    		res.json({
-    			message: "empty"
-    		})
-    	}
-    });
+	helperMethods.conn().query("SELECT shops.*, usershopbridge.uRole, (SELECT COUNT(*) FROM orders WHERE (shops.sID = orders.sID) AND (orders.oStatus = 'Waiting for order')) AS nOrders FROM shops, usershopbridge WHERE (shops.sID = usershopbridge.sID) AND (usershopbridge.uID = ?) AND (shops.isActive != 2) ORDER BY shops.sStatus DESC", [req.params.uID], (err, rows, fields) => {//WHERE uID=?", 
+		console.log(err);
+		console.log(rows);
+		if (rows.length > 0) {
+			res.json({
+				message: "shops",
+				shops: rows
+			})
+		} else {
+			res.json({
+				message: "empty"
+			})
+		}
+	});
 });
 
 //Returns all Menuitems for each shop
 helperMethods.router().get('/MenuItems/:sID', (req, res, next) => {
 	helperMethods.conn().query("SELECT * FROM `menuitems` WHERE sID = ? ORDER BY `mPrice` ASC", req.params.sID, (err, rows, fields) => {
 		console.log(err);
-        console.log(rows);
-        if (rows.length > 0) {
-        	res.json({
-        		message: "shops",
-        		menuItems: rows
-        	})
-        } else {
-        	res.json({
-        		message: "empty"
-        	})
-        }
-    });
+		console.log(rows);
+		if (rows.length > 0) {
+			res.json({
+				message: "shops",
+				menuItems: rows
+			})
+		} else {
+			res.json({
+				message: "empty"
+			})
+		}
+	});
 });
 
 //Returns all Ingredients for each shop
 helperMethods.router().get('/Ingredients/:sID', (req, res, next) => {
 	helperMethods.conn().query("SELECT ingredients.*, (SELECT COUNT(*) FROM `extras` WHERE sID = ?) AS nExtras FROM `ingredients` WHERE sID = ?", [req.params.sID, req.params.sID], (err, rows, fields) => {
 		console.log(err);
-         console.log(rows);
-        if (rows.length > 0) {
-        	res.json({
-        		message: "shops",
-        		ingredients: rows
-        	})
-        } else {
-        	res.json({
-        		message: "empty"
-        	})
-        }
-    });
+		console.log(rows);
+		if (rows.length > 0) {
+			res.json({
+				message: "shops",
+				ingredients: rows
+			})
+		} else {
+			res.json({
+				message: "empty"
+			})
+		}
+	});
 });
 
 //Returns all Extras for each shop
 helperMethods.router().get('/Extras/:sID', (req, res, next) => {
 	helperMethods.conn().query("SELECT * FROM `extras` WHERE sID = ?", [req.params.sID], (err, rows, fields) => {
 		console.log(err);
-          console.log(rows);
-        if (rows.length > 0) {
-        	res.json({
-        		message: "shops",
-        		extras: rows
-        	})
-        } else {
-        	res.json({
-        		message: "empty"
-        	})
-        }
-    });
+		console.log(rows);
+		if (rows.length > 0) {
+			res.json({
+				message: "shops",
+				extras: rows
+			})
+		} else {
+			res.json({
+				message: "empty"
+			})
+		}
+	});
 });
 
 //Register shop
 helperMethods.router().post('/Register', helperMethods.upload().fields([{ name: 'sSmallPicture' }, { name: 'sBigPicture' }]), (req, res, next) => {
-	const insQuery = "INSERT INTO shops(`sName`,`sShortDescrption`,`sFullDescription`, `sSmallPicture`, `sBigPicture`, `sLatitude`, `sLongitude`,`sRating`,`sStatus`,`sLikes`,`sOperatingHrs`,`sAddress`,`isActive`,`createdAt`) VALUES (?, ?,?, '"+
-	req.files.sSmallPicture[0].filename + "', '" + req.files.sBigPicture[0].filename + "', ?, ?, 0.0, 0,0,?,?,0, '" + helperMethods.createdAt() + "')";
+	const insQuery = "INSERT INTO shops(`sName`,`sShortDescrption`,`sFullDescription`, `sSmallPicture`, `sBigPicture`, `sLatitude`, `sLongitude`,`sRating`,`sReceiver`,`sStatus`,`sLikes`,`sOperatingHrs`,`sAddress`,`isActive`,`createdAt`) VALUES (?, ?,?, '" +
+		req.files.sSmallPicture[0].filename + "', '" + req.files.sBigPicture[0].filename + "', ?, ?, 0.0,?, 0,0,?,?,0, '" + helperMethods.createdAt() + "')";
 
 	helperMethods.conn().query(insQuery, [req.body.sName, req.body.sShortDescrption, req.body.sFullDescription,
-		req.body.sLatitude, req.body.sLongitude, req.body.sOperatingHrs,req.body.sAddress], (err, result, fields) => {
+	req.body.sLatitude, req.body.sLongitude, req.body.uID, req.body.sOperatingHrs, req.body.sAddress], (err, result, fields) => {
+		console.log(err);
+		console.log(result);
+		var sID = result.insertId;
+		const insQuery1 = "INSERT INTO usershopbridge(`uID`,`sID`,`uRole`, `createdAt`) VALUES (?, " + sID + ", 'Owner', '" + helperMethods.createdAt() + "')";
+		helperMethods.conn().query(insQuery1, [req.body.uID], (err, result, fields) => {
 			console.log(err);
 			console.log(result);
-			var sID = result.insertId;
-			const insQuery1 = "INSERT INTO usershopbridge(`uID`,`sID`,`uRole`, `createdAt`) VALUES (?, " + sID + ", 'Owner', '" + helperMethods.createdAt() + "')";
-			helperMethods.conn().query(insQuery1, [req.body.uID], (err, result, fields) => {
-				console.log(err);
-				console.log(result);
-				res.json({
-					data: sID
-				})
-			});
+			res.json({
+				data: sID
+			})
 		});
+	});
 });
 
 //Register shop Ingredients
@@ -121,12 +121,12 @@ helperMethods.router().post('/Register/Ingredient', (req, res, next) => {
 
 		helperMethods.conn().query(selQuery, result.insertId, (err, result, fields) => {
 			console.log(err);
-            console.log(result);
-            res.json({
-            	data: "saved",
-            	response: result
-            })
-        });
+			console.log(result);
+			res.json({
+				data: "saved",
+				response: result
+			})
+		});
 	});
 });
 
@@ -138,42 +138,42 @@ helperMethods.router().post('/Register/MenuItem', (req, res, next) => {
 	const selQuery = "SELECT * FROM `menuitems` WHERE mID = ?";
 	helperMethods.conn().query(insQuery, [req.body.mList, req.body.mPrice, req.body.sID], (err, result, fields) => {
 		console.log(err);
-                console.log(result);
-        var ID = result.insertId;
-        helperMethods.conn().query(checkQuesry, [req.body.sID], (err, result, fields) => {
-                        console.log(result);
-            console.log(err);
-            console.log(result[0].nItems);
-            if (result[0].nItems == 1) {
-            	helperMethods.conn().query(ActivateQuery, req.body.sID, (err, result, fields) => {
-            		console.log(err);
-            		console.log("isActive activated");
-                    console.log(result);
-                    helperMethods.conn().query(selQuery, ID, (err, result, fields) => {
-                    	console.log(err);
-                        console.log(result);
-                        res.json({
-                        	data: "saved",
-                        	response: result
-                        })
-                    });
-                });
+		console.log(result);
+		var ID = result.insertId;
+		helperMethods.conn().query(checkQuesry, [req.body.sID], (err, result, fields) => {
+			console.log(result);
+			console.log(err);
+			console.log(result[0].nItems);
+			if (result[0].nItems == 1) {
+				helperMethods.conn().query(ActivateQuery, req.body.sID, (err, result, fields) => {
+					console.log(err);
+					console.log("isActive activated");
+					console.log(result);
+					helperMethods.conn().query(selQuery, ID, (err, result, fields) => {
+						console.log(err);
+						console.log(result);
+						res.json({
+							data: "saved",
+							response: result
+						})
+					});
+				});
 
-            } else {
-            	helperMethods.conn().query(selQuery, ID, (err, result, fields) => {
-            		console.log("else");
-            		console.log(err);
-                    console.log(result);
-                    res.json({
-                    	data: "saved",
-                    	response: result
-                    })
-                });
+			} else {
+				helperMethods.conn().query(selQuery, ID, (err, result, fields) => {
+					console.log("else");
+					console.log(err);
+					console.log(result);
+					res.json({
+						data: "saved",
+						response: result
+					})
+				});
 
-            }
+			}
 
-        });
-    });
+		});
+	});
 });
 
 //Register shop Extras
@@ -185,12 +185,12 @@ helperMethods.router().post('/Register/Extra', (req, res, next) => {
 
 		helperMethods.conn().query(selQuery, result.insertId, (err, result, fields) => {
 			console.log(err);
-            console.log(result);
-            res.json({
-            	data: "saved",
-            	response: result
-            })
-        });
+			console.log(result);
+			res.json({
+				data: "saved",
+				response: result
+			})
+		});
 	});
 });
 
@@ -200,11 +200,11 @@ helperMethods.router().delete('/Register/Ingredient/:iID', (req, res, next) => {
 
 	helperMethods.conn().query(delQuery, [req.params.iID], (err, result, fields) => {
 		console.log(err);
-        console.log(result);
-        res.json({
-        	data: "removed"
-        })
-    });
+		console.log(result);
+		res.json({
+			data: "removed"
+		})
+	});
 });
 
 //Delete shop Menu items
@@ -216,35 +216,35 @@ helperMethods.router().delete('/Register/MenuItem/:mID/:sID', (req, res, next) =
 	var sID = req.params.sID;
 	helperMethods.conn().query(delQuery, [req.params.mID], (err, result, fields) => {
 		console.log(err);
-               console.log(result);
-        if (result == undefined) {
-        	res.err("something went wrong please try again")
-        } else {
-        	helperMethods.conn().query(checkQuesry, [sID], (err, result, fields) => {
-                   console.log(result);
-                console.log(err);
-                console.log(result[0].nItems);
-                if (result[0].nItems == 0) {
-                	helperMethods.conn().query(DeactivateQuery, [sID], (err, result, fields) => {
-                		console.log(sID);
-                		console.log(err);
-                		console.log("isActive deactivated");
-                        console.log(result);
-                        res.json({
-                        	data: "removed",
-                        	response: result
-                        })
-                    });
+		console.log(result);
+		if (result == undefined) {
+			res.err("something went wrong please try again")
+		} else {
+			helperMethods.conn().query(checkQuesry, [sID], (err, result, fields) => {
+				console.log(result);
+				console.log(err);
+				console.log(result[0].nItems);
+				if (result[0].nItems == 0) {
+					helperMethods.conn().query(DeactivateQuery, [sID], (err, result, fields) => {
+						console.log(sID);
+						console.log(err);
+						console.log("isActive deactivated");
+						console.log(result);
+						res.json({
+							data: "removed",
+							response: result
+						})
+					});
 
-                } else {
-                	res.json({
-                		data: "removed",
-                		response: result
-                	})
-                }
-            });
-        }
-    });
+				} else {
+					res.json({
+						data: "removed",
+						response: result
+					})
+				}
+			});
+		}
+	});
 });
 
 //Delete shop
@@ -256,20 +256,20 @@ helperMethods.router().delete('/Delete/:sID', (req, res, next) => {
 	var sID = req.params.sID;
 	helperMethods.conn().query(delIngredientsQuery, [req.params.sID], (err, result, fields) => {
 		console.log(err);
-                console.log(result);
-        helperMethods.conn().query(delLinkQuery,[req.params.sID], (err, result, fields) => {
-        	console.log(err);
-                    console.log(result);
-            helperMethods.conn().query(delShopQuery,[req.params.sID], (err, result, fields) => {
-            	console.log(err);
-                        console.log(result);
-                res.json({
-                	data: "removed",
-                	response: result
-                });
-            });
-        });
-    });
+		console.log(result);
+		helperMethods.conn().query(delLinkQuery, [req.params.sID], (err, result, fields) => {
+			console.log(err);
+			console.log(result);
+			helperMethods.conn().query(delShopQuery, [req.params.sID], (err, result, fields) => {
+				console.log(err);
+				console.log(result);
+				res.json({
+					data: "removed",
+					response: result
+				});
+			});
+		});
+	});
 });
 
 //Delete shop Extras
@@ -278,40 +278,40 @@ helperMethods.router().delete('/Register/Extra/:eID', (req, res, next) => {
 
 	helperMethods.conn().query(delQuery, [req.body.eID], (err, result, fields) => {
 		console.log(err);
-          console.log(result);
-        res.json({
-        	data: "removed"
-        })
-    });
+		console.log(result);
+		res.json({
+			data: "removed"
+		})
+	});
 });
 
 //Put shop
-helperMethods.router().put('/Register/:sID', helperMethods.upload().fields([{ name: 'sSmallPicture' }, { name: 'sBigPicture' }]),(req, res, next) => {
+helperMethods.router().put('/Register/:sID', helperMethods.upload().fields([{ name: 'sSmallPicture' }, { name: 'sBigPicture' }]), (req, res, next) => {
 	const putQuery = "UPDATE shops SET `sName` = ?,`sShortDescrption` = ?,`sFullDescription` = ?, `sSmallPicture` = '" + req.files.sSmallPicture[0].filename + "', `sBigPicture` =  '" + req.files.sBigPicture[0].filename + "', `sLatitude` = ?, `sLongitude` = ?, `sAddress` = ? WHERE sID = ?";
 
 	helperMethods.conn().query(putQuery, [req.body.sName, req.body.sShortDescrption, req.body.sFullDescription,
-		req.body.sLatitude, req.body.sLongitude, req.body.sAddress, req.params.sID], (err, result, fields) => {
-			console.log(err);
-			console.log(result);
-			res.json({
-				data: "changed"
-			})
-		});
+	req.body.sLatitude, req.body.sLongitude, req.body.sAddress, req.params.sID], (err, result, fields) => {
+		console.log(err);
+		console.log(result);
+		res.json({
+			data: "changed"
+		})
+	});
 });
 //Put shop
-helperMethods.router().put('/CompleteRegister/:sID', helperMethods.upload().fields([{ name: 'sSmallPicture' }, { name: 'sBigPicture' }]),(req, res, next) => {
+helperMethods.router().put('/CompleteRegister/:sID', helperMethods.upload().fields([{ name: 'sSmallPicture' }, { name: 'sBigPicture' }]), (req, res, next) => {
 	const putQuery = "UPDATE shops SET `sName` = ?,`sShortDescrption` = ?,`sFullDescription` = ?, `sSmallPicture` = '" + req.files.sSmallPicture[0].filename + "', `sBigPicture` =  '" + req.files.sBigPicture[0].filename + "', `sLatitude` = ?, `sLongitude` = ?, `sAddress` = ?, `sOperatingHrs` = ? WHERE sID = ?";
 
 	helperMethods.conn().query(putQuery, [req.body.sName, req.body.sShortDescrption, req.body.sFullDescription,
-		req.body.sLatitude, req.body.sLongitude, req.body.sAddress, req.body.sOperatingHrs, req.params.sID], (err, result, fields) => {
-			console.log(err);
-			console.log(result);
-			if (!err) {
-				res.json({
-					data: "changed"
-				})
-			}
-		});
+	req.body.sLatitude, req.body.sLongitude, req.body.sAddress, req.body.sOperatingHrs, req.params.sID], (err, result, fields) => {
+		console.log(err);
+		console.log(result);
+		if (!err) {
+			res.json({
+				data: "changed"
+			})
+		}
+	});
 });
 
 //Put operating hours
@@ -359,11 +359,11 @@ helperMethods.router().put('/Register/MenuItems/:mID', (req, res, next) => {
 
 	helperMethods.conn().query(putQuery, [req.body.mList, req.body.mPrice, req.params.mID], (err, result, fields) => {
 		console.log(err);
-         console.log(result);
-        res.json({
-        	data: "changed"
-        })
-    });
+		console.log(result);
+		res.json({
+			data: "changed"
+		})
+	});
 });
 
 //Put shop Extras
@@ -372,11 +372,11 @@ helperMethods.router().put('/Register/Extra/:eID', (req, res, next) => {
 
 	helperMethods.conn().query(putQuery, [req.body.eName, req.params.eID], (err, result, fields) => {
 		console.log(err);
-         console.log(result);
-        res.json({
-        	data: "saved"
-        })
-    });
+		console.log(result);
+		res.json({
+			data: "saved"
+		})
+	});
 });
 
 //Put shop Status
@@ -385,11 +385,11 @@ helperMethods.router().put('/Status/:sID', (req, res, next) => {
 
 	helperMethods.conn().query(putQuery, [req.body.sStatus, req.params.sID], (err, result, fields) => {
 		console.log(err);
-    console.log(result);
-        res.json({
-        	data: "saved"
-        })
-    });
+		console.log(result);
+		res.json({
+			data: "saved"
+		})
+	});
 });
 
 //Deactivate Shop
@@ -398,11 +398,11 @@ helperMethods.router().put('/deactivate/:sID', (req, res, next) => {
 
 	helperMethods.conn().query(putQuery, [req.params.sID], (err, result, fields) => {
 		console.log(err);
-         console.log(result);
-        res.json({
-        	data: "deactivated"
-        })
-    });
+		console.log(result);
+		res.json({
+			data: "deactivated"
+		})
+	});
 });
 
 module.exports = helperMethods.router();
