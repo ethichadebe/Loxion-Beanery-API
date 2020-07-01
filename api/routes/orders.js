1d4ffe904dcc6412d18c5a6c54c60ccf260ec3e0
Ethic Mthobisi Hadebe <ethichadebe@gmail.com>
2020-07-01 07:30:38 +0200

    Updates: Order complete push notification

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -28,7 +28,7 @@ helperMethods.router().post('/Order', (req, res, next) => {
                         },
 
                         "data": {
-                            "oID": "" + result[0].oID,
+                            "oID": "" + req.params.oID,
                             "sID": "" + result[0].sID,
                             "sName": "" + result[0].sName,
                             "sSmallPicture": "" + result[0].sSmallPicture,
@@ -196,7 +196,7 @@ helperMethods.router().put('/Arrived/:oID', (req, res, next) => {
                     },
 
                     "data": {
-                        "oID": "" + result[0].oID,
+                        "oID": "" + req.params.oID,
                         "sID": "" + result[0].sID,
                         "sName": "" + result[0].sName,
                         "sSmallPicture": "" + result[0].sSmallPicture,
