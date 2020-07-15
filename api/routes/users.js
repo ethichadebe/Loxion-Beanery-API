a59bf6294d78709d6db0e83e8b798065cfd48b0c
Ethic Mthobisi Hadebe <ethichadebe@gmail.com>
2020-07-16 01:43:25 +0200

    Change of database

--- api/routes/users.js
+++ api/routes/users.js
@@ -207,6 +207,29 @@ helperMethods.router().put('/EditProfile', helperMethods.upload().single('Profil
 
 });
 
+//Update user profile picture
+helperMethods.router().put('/EditProfilePicture', helperMethods.upload().single('ProfilePicture'), (req, res, next) => {
+    console.log(req.file);
+    const updateQuery = "UPDATE users SET uPicture = '" + req.file.location + "' WHERE uID = ?";
+
+    //Check if number and email exists then register 
+    helperMethods.conn().query(updateQuery, [req.body.uID], (err, result, fields) => {
+        console.log(err);
+        const selectQuery = "SELECT * FROM `users` WHERE  `uID` = ?";
+        var uID = req.body.uID;
+        console.log(uID);
+        helperMethods.conn().query(selectQuery, [uID], (err, result, fields) => {
+            console.log(err);
+            console.log(result);
+            res.json({
+                data: "saved",
+                response: result
+            })
+        });
+    });
+
+});
+
 //Update user password
 helperMethods.router().put('/ChangePassword/:uID', (req, res, next) => {
     const updateQuery = "UPDATE users SET uPassword = ? WHERE uID = ?";
